d86942ef445af55411d77683376d176f1d716a9c
aritsarann <158459428+aritsarann@users.noreply.github.com>
2025-01-26 07:27:55 +0000

    edit

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import random
 
+st.set_page_config(layout="wide")
 st.title('Test Streamlit')
-st.write('Hello! LMS')
+st.write('Lose Media Society')
+
+result1 = st.button("click me!")
+if result1 :
+    st.write("you click 1")
+
+result2 = st.button("click me!!", type="tertiary")
+if result1 & result2:
+    st.write("you click both")
+
+st.button("reset", type="primary")
 
 if st.button('Generate Random Number'):
     random_number = random.randint(1, 100)
